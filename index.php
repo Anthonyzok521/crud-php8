0c6d87b7ff8d723d5b22713d0d4f5df54e3b2942
anthonyzok521 <anthonyzok521@gmail.com>
2024-03-22 11:12:32 -0400

    Base code

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    //HELLO WORLD!
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD - PHP 8</title>
+</head>
+<body>
+    <h1>HELLO WORLD!</h1>
+</body>
+</html>
